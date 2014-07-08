2bff9d15cfb02f6ae3e821122f54ce057ded7142
Nick DeChant <nick.dechant@utexas.edu>
2014-07-07 19:11:31 -0500

    models skeleton

--- Twistory/Twistory/myapp/models.py
+++ Twistory/Twistory/myapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Handle(models.Model) :
+	picture = models.ImageField()
+	name = models.CharField(max_length = 50)
+	video = video_file = models.FileField(upload_to = u'video/', max_length=200)
+	bio = models.CharField(max_length = 100)
+	graph = models.ImageField()
+	tweets = models.TextField(null = True)
+
+class Hashtag(models.Model) :
+	name = models.CharField(max_length = 50)
+	graph = models.ImageField()
+	description = models.CharField(max_length = 50)
+	popular_users = models.TextField(null = True)
+
+class Cluster(models.Model) :
+	name = models.CharField(max_length = 50)
+	members = models.TextField(null = True)
+	parents = models.CharField(max_length = 50)
+	timeline = models.ImageField()
+	description = models.CharField(max_length = 100)
+	key_figures = models.TextField(null = True)
+	related_pages = models.TextField(null = True)
+	graphs = models.ImageField()
