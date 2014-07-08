5b15d73bb75ab9aeb88fb842368bcaca8ead1c62
Nick DeChant <nick.dechant@utexas.edu>
2014-07-07 20:26:05 -0500

    Fixed indentations.

--- Twistory/Twistory/myapp/views.py
+++ Twistory/Twistory/myapp/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 import datetime
 
 def hello(request):
-    return HttpResponse("Hello world")
+	return HttpResponse("Hello world")
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    return render(request, 'current_datetime.html', {'current_date' : now })
+	now = datetime.datetime.now()
+	return render(request, 'current_datetime.html', {'current_date' : now })
 
 def homepage(request):
 	image_data = open("/home/Twistory/Twistory/Twistory/myapp/ChrisBosh.jpg", "rb").read()
-    return render(request, 'current_datetime.html', {'current_date' : image_data })
+	return render(request, 'current_datetime.html', {'current_date' : image_data })
