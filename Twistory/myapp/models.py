2066e5c062baf3704ad812e167f1eca328a69b81
Nick DeChant <nick.dechant@utexas.edu>
2014-07-21 19:56:14 -0500

    Updated models with __str__ functions

--- Twistory/myapp/models.py
+++ Twistory/myapp/models.py
@@ -13,6 +13,9 @@ class State(models.Model) :
     population = models.IntegerField()
     size = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Park(models.Model) :
     """
     Model class for Park. Used to represent Park data.
@@ -27,6 +30,9 @@ class Park(models.Model) :
     num_visitors = models.IntegerField()
     #video
 
+    def __str__(self):
+        return self.name
+
 class Hike(models.Model) :
     """
     Model class for Hikes.  Used to represent Hike data.
@@ -37,3 +43,6 @@ class Hike(models.Model) :
     hike_image = models.ImageField("hike_image", upload_to="images/")
     difficulty = models.CharField(max_length = 255)
     park = models.ForeignKey("Park", related_name = "hikes")
+
+    def __str__(self):
+        return self.name
