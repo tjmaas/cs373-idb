0d84d0b5561e023a22ebc0a3023b72bf72a27a8f
Nick DeChant <nick.dechant@utexas.edu>
2014-07-23 20:15:00 -0500

    First database test

--- Twistory/myapp/models.py
+++ Twistory/myapp/models.py
@@ -8,10 +8,10 @@ class State(models.Model) :
     """
     name = models.CharField(max_length = 255)
     date_founded = models.CharField(max_length = 10)
-    flag = models.ImageField("Flag", upload_to="images/")
-    #flag = models.CharField(max_length = 1000)
+    flag = models.CharField(max_length = 1000)
     population = models.IntegerField()
     size = models.IntegerField()
+    video = models.CharField(max_length = 1000)
 
     def __str__(self):
         return self.name
@@ -24,11 +24,10 @@ class Park(models.Model) :
     name = models.CharField(max_length = 255)
     size = models.IntegerField()
     max_elevation = models.IntegerField()
-    date_founded = models.IntegerField()
-    park_image = models.ImageField("park_map", upload_to="images/")
-    #park_image = models.CharField(max_length = 1000)
+    date_founded = models.CharField(max_length = 255)
+    park_image = models.CharField(max_length = 1000)
     num_visitors = models.IntegerField()
-    #video
+    video = models.CharField(max_length = 1000)
 
     def __str__(self):
         return self.name
@@ -40,9 +39,9 @@ class Hike(models.Model) :
     name = models.CharField(max_length = 255)
     distance = models.FloatField()
     est_time = models.IntegerField()
-    hike_image = models.ImageField("hike_image", upload_to="images/")
+    hike_image = models.CharField(max_length = 1000)
     difficulty = models.CharField(max_length = 255)
-    park = models.ForeignKey("Park", related_name = "hikes")
+    park = models.ForeignKey('Park', related_name = "hikes")
 
     def __str__(self):
         return self.name
